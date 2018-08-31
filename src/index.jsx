ad14062d27f7f08c05aa750a5b812c3f342dc395
josephtan <tantjernbit@gmail.com>
2018-08-31 15:03:16 +1000

    add index rendering page

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter} from "react-router-dom";
+import App from "./components/app";
+import StarBackground from "./components/starbackground";
+import "./css/style.scss";
+
+ReactDom.render(<BrowserRouter>
+    <App />
+    </BrowserRouter>, document.querySelector("#app"));

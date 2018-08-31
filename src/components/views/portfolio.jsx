0df55170ef9a49731b8240b96ff4cb497790d407
josephtan <tantjernbit@gmail.com>
2018-08-31 15:13:09 +1000

    add dummy page portfolio

---
+++ src/components/views/portfolio.jsx
@@ -0,0 +1,8 @@
+const Portfolio=()=> (
+    <div>
+        <h2>Profile</h2>
+        <p>Aenean accumsan justo in porta elementum. Ut purus dolor, semper id purus nec, condimentum auctor mi. Morbi viverra ultricies dolor id volutpat. Pellentesque non lorem imperdiet nunc scelerisque volutpat. </p>
+    </div>
+);
+
+export default Portfolio;

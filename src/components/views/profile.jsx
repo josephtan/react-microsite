ae89a63a448188452a0637269488730840f0c828
josephtan <tantjernbit@gmail.com>
2018-08-31 15:07:40 +1000

    add profile page dummy

---
+++ src/components/views/profile.jsx
@@ -0,0 +1,9 @@
+
+const Profile=()=> (
+    <div>
+        <h2>Profile</h2>
+        <p>Aenean accumsan justo in porta elementum. Ut purus dolor, semper id purus nec, condimentum auctor mi. Morbi viverra ultricies dolor id volutpat. Pellentesque non lorem imperdiet nunc scelerisque volutpat. </p>
+    </div>
+);
+
+export default Profile;

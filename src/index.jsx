b7960e0be5b9215c65849c7d78799ec98d96fd50
josephtan <tantjernbit@gmail.com>
2018-09-04 17:10:02 +1000

    remove starbackground module, conflict with p5 wrapper

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter} from "react-router-dom";
 import App from "./components/app";
-import StarBackground from "./components/starbackground";
 import "./css/app.scss";
 
 ReactDom.render(<BrowserRouter>

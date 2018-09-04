14a105d2d1ec90c022afda5cef1bfd2f833dd648
josephtan <tantjernbit@gmail.com>
2018-09-04 23:05:58 +1000

    add d3 module, add new hexagon rendering function to componentDidMount

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,6 +3,7 @@ import React, { Component} from "react";
 import { NavLink } from 'react-router-dom';
 import PropTypes from "prop-types";
 import client from "./client";
+import * as d3 from "d3";
 
 // =========
 // NAV ITEMS
@@ -24,8 +25,8 @@ export default class Navigation extends Component{
             }).catch((err)=>{
             console.log(err);
         });
+       this.renderHex();
     }
-
     toggleShow (){
         const currentState = this.state.active;
         const currentNav = this.state.showNav;
@@ -41,6 +42,28 @@ export default class Navigation extends Component{
     clickHandle(){
         this.toggleShow();
     }
+    renderHex(){
+        let margins = {top: -22, left: -22};
+        let _sq32 = (Math.sqrt(3) / 2), menuRad = 24, menX = 48, menY = 48;
+        const drawHexagon =  d3.line()
+            .x(function(d) { return d.x; })
+            .y(function(d) { return d.y; })
+            .curve(d3.curveCardinalClosed.tension(1));
+        const hexagonData = [
+            { "x": menuRad + menX, "y": menY},
+            { "x": menuRad / 2 + menX,  "y": menuRad * _sq32 + menY},
+            { "x": -menuRad / 2 + menX,  "y": menuRad * _sq32 + menY},
+            { "x": -menuRad + menX,  "y": menY},
+            { "x": -menuRad / 2 + menX,  "y": -menuRad * _sq32 + menY},
+            { "x": menuRad / 2 + menX, "y": -menuRad * _sq32 + menY}];
+        let  strokeC = "#00e3ff", strokeW = 2, fill = "transparent";
+        let menuBtn = d3.select(".menuBtnElements");
+        let menuBtnElements = menuBtn.select("path")
+            .attr("d", drawHexagon(hexagonData))
+            .attr("transform", "translate(" + [margins.left, margins.top] + ")")
+            .attr("fill", fill)
+            .attr("stroke-width", strokeW);
+    }
     render() {
         const navLink = this.state.navdata.map((navlink) => {
             if (this.state.show) {
@@ -51,8 +74,9 @@ export default class Navigation extends Component{
             <nav>
 
                     <div className={this.state.active ? "dropdown active" : "dropdown"}>
-                        <button className={this.state.active ? "menuBtn dropdown-btn hamburger hamburger--spin is-active": "menuBtn dropdown-btn hamburger hamburger--spin"} onClick={this.clickHandle.bind(this)}>
-                                  <span className="hamburger-box">
+                        <button className={this.state.active ? "menuBtn dropdown-btn hamburger hamburger--spin is-active":"menuBtn dropdown-btn hamburger hamburger--spin"} onClick={this.clickHandle.bind(this)}>
+                            <svg className={"menuBtnElements"}><path></path></svg>
+                              <span className="hamburger-box">
                                   <span className="hamburger-inner"></span>
                                 </span>
                         </button>

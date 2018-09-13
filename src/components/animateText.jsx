cb2850d7a37a12d3957649028de1d77e86c974f5
josephtan <tantjernbit@gmail.com>
2018-09-13 23:41:06 +1000

    add additional condition to span Lettering text where matching white spaces adds an &nbsp within the span as react doesn't render HTML entities. Add anime.js text animation after tags are spanned as the wrapper looks for letters in HTML tags.

--- src/components/animateText.jsx
+++ src/components/animateText.jsx
@@ -6,9 +6,9 @@ import {anime} from "animejs";
 export default class animateText extends Component{
     constructor(props){
         super(props);
-        this.title = "Front End Developer";
-        this.title2 = "Front End Engineer";
-        this.title3 = "Fullstack Developer";
+        this.title = "Front End Engineer";
+        this.title2 = "Fullstack Developer";
+        this.description = "For the simplicity of things my profession involves in making things dance on the screen.";
         this.aniOpacity = [0,1];
         this.scaleX = [4,1];
     }
@@ -18,7 +18,12 @@ export default class animateText extends Component{
     spanLettering(string){
         let stringArray = string.split("");
         const letters = stringArray.map((text,i)=>{
-            return (<span key={i} className="letter">{text}</span>);
+            if(text.match(/\s+/g) != null){
+                return (<span key={i} className="letter">&nbsp;</span>);
+            }else{
+                return (<span key={i} className="letter">{text}</span>);
+            }
+
         });
         return letters;
 
@@ -29,23 +34,22 @@ export default class animateText extends Component{
         fsd.duration =[800];
         fsd.durationOut= [600];
         return(
-            <div>
-                <p className="fed has-text-centered">
+            <div className="block__relative has-text-centered">
+                <h3 className="fed">
                     <Anime loop={true} scaleX={this.scaleX} opacity={this.aniOpacity} easing={"easeOutExpo"} duration={950} direction="alternate" delay={(el, index) => index * 80} >
                         {this.spanLettering(this.title)}
                     </Anime>
-
-                </p>
-                <p className="fee has-text-centered">
-                    <Anime loop={true} scaleX={this.scaleX} translateY={"2em"} opacity={this.aniOpacity} easing={"easeOutExpo"} direction="alternate" duration={1200} delay={(el, index) => index * 180} >
+                </h3>
+                <h3 className="fsd">
+                    <Anime loop={true} scale={fsd.scaleIn} elasticity={600} duration={fsd.duration} direction="alternate" delay={(el, index) => {return 45 * (index+ 1)}}>
                         {this.spanLettering(this.title2)}
                     </Anime>
-                </p>
-                <p className="fsd has-text-centered">
-                    <Anime loop={true} scale={fsd.scaleIn} elasticity={600} duration={fsd.duration} direction="alternate" delay={(el, index) => {return 45 * (index+ 1)}}>
-                        {this.spanLettering(this.title3)}
+                 </h3>
+                <p className="description">
+                    <Anime loop={true} translateX={[40,0]} opacity={[0,1]} easing={"easeOutExpo"} duration={1400} delay={(el, index) => {return 500  + (30 * index)}}>
+                    {this.spanLettering(this.description)}
                     </Anime>
-                 </p>
+                </p>
             </div>
         )
     }

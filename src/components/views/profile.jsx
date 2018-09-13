b4ee22bbeb073c1946a2caac589917e1d7e49ce7
josephtan <tantjernbit@gmail.com>
2018-09-13 23:48:59 +1000

    update profile page and description on what I do

--- src/components/views/profile.jsx
+++ src/components/views/profile.jsx
@@ -1,8 +1,27 @@
+import AnimateText from "../animateText";
+import Anime from "react-anime";
+import React from "react";
 
+let aniText = new AnimateText();
+let description = "I am a professional full-stack developer that primarily builds websites on PHP based open source CMS and moving towards node.js. My variety of work involvement has grown my skills over to web design, "
+    +"accessibility and basic server administration.";
 const Profile=()=> (
-    <div>
+    <div className="page-profile has-text-centered">
         <h2>Profile</h2>
-        <p>Aenean accumsan justo in porta elementum. Ut purus dolor, semper id purus nec, condimentum auctor mi. Morbi viverra ultricies dolor id volutpat. Pellentesque non lorem imperdiet nunc scelerisque volutpat. </p>
+        <p className="med-width">
+            "I am a professional full-stack developer that primarily builds websites on PHP based open source CMS and moving towards node.js. My variety of work involvement has grown my skills over to web design,
+            accessibility and basic server administration."
+        </p>
+        <h4> What have I done on my experience so far? </h4>
+        <ul className="has-text-left">
+            <li>Build &amp; design sites from scratch for different devices. These can be for small businesses to large enterprises within teams or myself.</li>
+            <li>Building small to large complex forms with custom validation checking, and integrate them to payment systems / micro-services and have user details into database SQL tables within teams or myself.</li>
+            <li>Build a simple query form to fetch user details and display them on the page.</li>
+            <li>Build technical solutions that require little knowledge for non-technical personnel for updates and maintenance.</li>
+            <li>Rewrite urls with htaccess on apache servers (for both security and SEO purposes).</li>
+            <li>Setting up site accounts, email and hosting through Apache cpanel and rewrite URLS through DNS.</li>
+            <li>More to come &hellip;</li>
+        </ul>
     </div>
 );
 

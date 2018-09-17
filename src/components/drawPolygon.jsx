e77de9f2fcee23e33e9bdf2f22d3d95ccc6aa453
josephtan <tantjernbit@gmail.com>
2018-09-17 15:48:29 +1000

    new component drawPolygon

---
+++ src/components/drawPolygon.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import * as d3 from "d3";
+
+export default class DrawPolygon extends Component {
+    constructor(props){
+     super(props);
+     this.polygonSelector = ".draw-hex";
+     this.marginTop = -12;
+     this.marginLeft = -12;
+     this.hexWidth = 66;
+     this.hexHeight = 66;
+     this.hexRadius =  52;
+    }
+    componentDidMount(){
+        this.renderHex(this.polygonSelector);
+    }
+    renderHex(polygon){
+        let margins = {top:this.marginTop, left:this.marginLeft};
+        let _sq32 = (Math.sqrt(3) / 2), hexRad = this.hexRadius, hexX = this.hexWidth , hexY = this.hexHeight;
+        const drawHexagon =  d3.line()
+            .x(function(d) { return d.x; })
+            .y(function(d) { return d.y; })
+            .curve(d3.curveCardinalClosed.tension(1));
+        const hexagonData = [
+            { "x": hexRad + hexX,"y": hexY},
+            { "x": hexRad / 2 + hexX,"y": hexRad * _sq32 + hexY},
+            { "x": -hexRad / 2 + hexX,"y": hexRad * _sq32 + hexY},
+            { "x": -hexRad + hexX,"y": hexY},
+            { "x": -hexRad / 2 + hexX,"y": -hexRad * _sq32 + hexY},
+            { "x": hexRad / 2 + hexX,"y": -hexRad * _sq32 + hexY}];
+        let strokeW = 2, fill = "transparent";
+        let polygonSelector = d3.selectAll(polygon).append("svg");
+             polygonSelector.append("path")
+            .attr("class", "hex-path")
+            .attr("d", drawHexagon(hexagonData))
+            .attr("transform", "translate(" + [margins.left, margins.top] + ")")
+            .attr("fill", fill)
+            .attr("stroke-width", strokeW);
+    }
+    render(){
+        return(
+            <div className="block__relative home-skillsets">
+                <div className="draw-hex">
+                    <span className="font-fa fa-github"></span>
+                </div>
+                <div className="draw-hex">
+                    <span className="font-fa fa-github"></span>
+                </div>
+                <div className="draw-hex">
+                    <span className="font-fa fa-github"></span>
+                </div>
+            </div>
+        );
+    }
+}

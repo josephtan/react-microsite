27509d2fced8d3cbbd590f11b32bdb52037cfb1f
josephtan <tantjernbit@gmail.com>
2018-09-17 15:48:05 +1000

    add drawPolygon module

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -1,8 +1,11 @@
 import AnimateText from "../animateText";
+import DrawPolygon from "../drawPolygon";
+
 
 const Home=()=> (
      <div className="page-home">
        <AnimateText/>
+       <DrawPolygon />
      </div>
 );
 

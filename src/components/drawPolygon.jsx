6d8d5e4465ae4c529078b05c7a8fdf2242d1800b
josephtan <tantjernbit@gmail.com>
2018-10-09 13:26:16 +1100

    add extra svg polygon function for hexagon line mask

--- src/components/drawPolygon.jsx
+++ src/components/drawPolygon.jsx
@@ -10,9 +10,16 @@ export default class DrawPolygon extends Component {
      this.hexWidth = 66;
      this.hexHeight = 66;
      this.hexRadius =  52;
+     this.maskSelector = ".masked-div";
+     this.maskmarginTop = -12;
+     this.maskmarginLeft = -14;
+     this.maskhexWidth = 64;
+     this.maskhexHeight = 64;
+     this.maskhexRadius =  50;
     }
     componentDidMount(){
         this.renderHex(this.polygonSelector);
+        this.renderHexMask(this.maskSelector)
     }
     renderHex(polygon){
         let margins = {top:this.marginTop, left:this.marginLeft};
@@ -31,14 +38,38 @@ export default class DrawPolygon extends Component {
         let strokeW = 2, fill = "transparent";
         let polygonSelector = d3.selectAll(polygon).append("svg");
              polygonSelector.attr("preserveAspectRatio","none");
-             polygonSelector.append("clipPath")
+             polygonSelector.append("defs")
+            .append("clipPath")
             .attr("id","maskPath")
             .append("path")
             .attr("class", "hex-path")
             .attr("d", drawHexagon(hexagonData))
-            .attr("transform", "translate(" + [margins.left, margins.top] + ")")
-            .attr("fill", fill)
-            .attr("stroke-width", strokeW);
+            .attr("transform", "translate(" + [margins.left, margins.top] + ")");
+    }
+    renderHexMask(mask){
+        let margins = {top:this.maskmarginTop, left:this.maskmarginLeft};
+        let _sq32 = (Math.sqrt(3) / 2), hexRad = this.maskhexRadius, hexX = this.maskhexWidth , hexY = this.maskhexHeight;
+        const drawHexagon =  d3.line()
+            .x(function(d) { return d.x; })
+            .y(function(d) { return d.y; })
+            .curve(d3.curveCardinalClosed.tension(1));
+        const hexagonData = [
+            { "x": hexRad + hexX,"y": hexY},
+            { "x": hexRad / 2 + hexX,"y": hexRad * _sq32 + hexY},
+            { "x": -hexRad / 2 + hexX,"y": hexRad * _sq32 + hexY},
+            { "x": -hexRad + hexX,"y": hexY},
+            { "x": -hexRad / 2 + hexX,"y": -hexRad * _sq32 + hexY},
+            { "x": hexRad / 2 + hexX,"y": -hexRad * _sq32 + hexY}];
+        let strokeW = 2, fill = "transparent";
+        let maskSelector = d3.selectAll(mask).append("svg");
+        maskSelector.attr("preserveAspectRatio","none");
+        maskSelector.append("defs")
+            .append("clipPath")
+            .attr("id","hexMask")
+            .append("path")
+            .attr("class", "hex-path")
+            .attr("d", drawHexagon(hexagonData))
+            .attr("transform", "translate(" + [margins.left, margins.top] + ")");
     }
     render(){
         return(

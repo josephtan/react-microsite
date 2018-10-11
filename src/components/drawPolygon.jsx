af406c564d859eaa5371fe74b09731f75ec24244
josephtan <tantjernbit@gmail.com>
2018-10-11 15:56:03 +1100

    add classCycler function that goes to react refs elements

--- src/components/drawPolygon.jsx
+++ src/components/drawPolygon.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import * as d3 from "d3";
 
 export default class DrawPolygon extends Component {
@@ -16,10 +17,35 @@ export default class DrawPolygon extends Component {
      this.maskhexWidth = 64;
      this.maskhexHeight = 64;
      this.maskhexRadius =  50;
+     this.cycleinterval = 2000;
+     this.cycleinterval2 = 2000;
+     this.cycleinterval3 = 2000;
     }
     componentDidMount(){
+        let classCyclerSelector = ReactDOM.findDOMNode(this.refs.logoList);
+        let classCyclerSelector2 = ReactDOM.findDOMNode(this.refs.logoList2);
+        let classCyclerSelector3 = ReactDOM.findDOMNode(this.refs.logoList3);
         this.renderHex(this.polygonSelector);
-        this.renderHexMask(this.maskSelector)
+        this.renderHexMask(this.maskSelector);
+        this.classCycler(classCyclerSelector, this.cycleinterval);
+        this.classCycler(classCyclerSelector2, this.cycleinterval2);
+        this.classCycler(classCyclerSelector3, this.cycleinterval3);
+
+    }
+    classCycler(classSelector, cycleintervals){
+        let countlist = classSelector.childNodes,
+            listlength = countlist.length,
+            interval = cycleintervals,
+            currentList = -1,
+            itemList = countlist;
+            window.setInterval(function () {
+           if( itemList[currentList] != undefined && itemList[currentList].classList.contains("motion")){
+               itemList[currentList].classList.remove("motion");
+           }
+            currentList++;
+            if(currentList == listlength) currentList = 0;
+            itemList[currentList].classList.add("motion");
+        },Math.floor(Math.random() * 3000) + interval);
     }
     renderHex(polygon){
         let margins = {top:this.marginTop, left:this.marginLeft};
@@ -71,22 +97,42 @@ export default class DrawPolygon extends Component {
     }
     render(){
         return(
+            <div>
             <div className="block__relative home-skillsets">
                 <div className="draw-hex">
                     <div className="masked-div">
-                        <span className="font-fa fa-github"></span>
+                        <ul className="logo-list" ref="logoList">
+                            <li><span className="font-fa fa-github"></span></li>
+                            <li><span className="font-fa-brands fa-npm"></span></li>
+                            <li><span className="font-fa-brands fa-aws"></span></li>
+                            <li><span className="font-fa-brands fa-paypal"></span></li>
+                            <li><span className="font-fa-brands fa-php"></span></li>
+                        </ul>
                     </div>
                 </div>
                 <div className="draw-hex">
                     <div className="masked-div">
-                    <span className="font-fa fa-github"></span>
+                        <ul className="logo-list" ref="logoList2">
+                            <li><span className="font-fa-brands fa-html5"></span></li>
+                            <li><span className="font-fa-brands fa-css3"></span></li>
+                            <li><span className="font-fa-brands fa-sass"></span></li>
+                        </ul>
                     </div>
                 </div>
                 <div className="draw-hex">
                     <div className="masked-div">
-                        <span className="font-fa fa-github"></span>
+                        <ul className="logo-list" ref="logoList3">
+                            <li><span className="font-fa-brands fa-js"></span></li>
+                            <li><span className="font-fa-brands fa-node"></span></li>
+                            <li><span className="font-fa-brands fa-react"></span></li>
+                            <li><span className="font-fa-brands fa-grunt"></span></li>
+                            <li><span className="font-fa-brands fa-gulp"></span></li>
+                            <li><span className="font-fa-brands fa-angular"></span></li>
+                        </ul>
                     </div>
                 </div>
+            </div>
+                <p className="has-text-centered">Technologies I have had hands on experience are animated above.</p>
             </div>
         );
     }

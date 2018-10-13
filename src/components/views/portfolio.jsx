6be0370a3a0d8f008d871fcc675e68f614add421
josephtan <tantjernbit@gmail.com>
2018-10-13 14:11:02 +1100

    add PopUpBox component to portfolio page

--- src/components/views/portfolio.jsx
+++ src/components/views/portfolio.jsx
@@ -1,7 +1,10 @@
+import popUpBox from "../PopupBox";
+import PopupBox from "../popupBox";
 const Portfolio=()=> (
     <div className="page-portfolio has-text-centered">
         <h2>Portfolio</h2>
         <p>Pellentesque non lorem imperdiet nunc scelerisque volutpat. </p>
+        <PopupBox/>
     </div>
 );
 

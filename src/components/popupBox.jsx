1a01f2b3a69da976b38096815c469159546d657c
josephtan <tantjernbit@gmail.com>
2018-10-15 18:59:41 +1100

    add tooltip and mouseOver, mouseOut functions

--- src/components/popupBox.jsx
+++ src/components/popupBox.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import axiosOption from "./axiosOption";
 import {drawPolygon} from "./drawPolygon";
 
 export default class PopupBox extends Component{
     constructor(props){
         super(props);
-        this.state = {showPopUp:this.props.showPopUp, portfoliodata:[]};
+        this.state = {currentPopUp:this.props.currentPopUp, activePopUp:this.props.activePopUp, thisMouseOver:this.props.thisMouseOver, portfoliodata:[]};
+        this.wrapperSelector =".wrapper";
+        this.overlaySelector = ".menu-overlay";
+        this.openMenuSelector = "open-menu";
+        this.activeClass = "active";
     }
     componentDidMount(){
         axiosOption.get("portfoliodata.json")
@@ -23,17 +26,43 @@ export default class PopupBox extends Component{
     }
     togglePopUp(items){
         const currentState = this.state.active;
-        this.setState({ active: !currentState,showPopUp: items.index});
-    }
+        const activePopup = this.state.activePopUp;
+        const currentPopUp = items.index;
+        this.setState({ active: !currentState, activePopUp: !activePopup, currentPopUp: currentPopUp});
 
+        if(activePopup === true) {
+            document.querySelector(this.wrapperSelector).classList.add(this.openMenuSelector);
+            document.querySelector(this.overlaySelector).classList.add(this.activeClass);
+        }else{
+            document.querySelector(this.wrapperSelector).classList.remove(this.openMenuSelector);
+            document.querySelector(this.overlaySelector).classList.remove(this.activeClass);
+        }
+    }
+    mouseOver(items){
+        document.querySelector(".popup-tooltip").classList.remove("slidedown");
+       const currentPopUp = items.index;
+       this.setState({thisMouseOver: true, currentPopUp:currentPopUp});
+
+    }
+    mouseOut(items){
+        const currentPopUp = items.index;
+        this.setState({thisMouseOver:false,currentPopUp:currentPopUp});
+        document.querySelector(".popup-tooltip").classList.add("slidedown");
+    }
     render(){
         const itemList = this.state.portfoliodata.map((items,i) => {
-            return(<li key={i} onClick={this.clickHandle.bind(this,items)}>
-                <img className="thumbnail" src={require("../images/".concat(items.thumb))} alt={items.text} />
-                <div className={this.state.active === true && this.state.showPopUp === items.index  ? "popup active" : "popup"}>
+            return(<li key={i}>
+                <a href="#" key={i} onClick={this.clickHandle.bind(this,items)} onMouseOver={this.mouseOver.bind(this,items)} onMouseOut={this.mouseOut.bind(this,items)}>
+                    <img className="thumbnail" src={require("../images/".concat(items.thumb))} alt={items.text} />
+                    <div className={this.state.thisMouseOver === true && this.state.currentPopUp === items.index ? "popup-tooltip slideup":"popup-tooltip"}><span className="has-text-centered">{items.text}</span></div>
+                </a>
+                <div className={this.state.active === true && this.state.currentPopUp === items.index  ? "popup active" : "popup"}>
                     <div className="popup-content">
-                        <button onClick={this.clickHandle.bind(this,items)}>Close</button>
+                        <button onClick={this.clickHandle.bind(this,items)}>
+                            <span className="font-fa-solid fa-times is-borderless"></span>
+                        </button>
                         <img src={require("../images/".concat(items.image))} alt={items.text} width="800" height="600" />
+                        <p>{items.description}</p>
                     </div>
                 </div>
             </li>);
@@ -46,3 +75,7 @@ export default class PopupBox extends Component{
             </div>
         )
     }}
+PopupBox.defaultProps = {
+    activePopUp: true,
+    thisMouseOver: false
+};

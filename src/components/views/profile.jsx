b5b3157e9b7d7a6bdeb912d7dfb6268e0dd83d0f
josephtan <tantjernbit@gmail.com>
2018-10-18 11:13:34 +1100

    wording update

--- src/components/views/profile.jsx
+++ src/components/views/profile.jsx
@@ -12,7 +12,7 @@ const Profile=()=> (
         <h4> What have I done on my experience so far? </h4>
         <ul className="has-text-left">
             <li>Build &amp; design sites from scratch for different devices. These can be for small businesses to large enterprises within teams or myself.</li>
-            <li>Building small to large complex forms with custom validation checking, and integrate them to payment systems / micro-services and have user details into database SQL tables within teams or myself.</li>
+            <li>Building small to large complex forms with custom validation checking, and integrate them to payment systems / micro-services and have user details written into SQL tables.</li>
             <li>Build a simple query form to fetch user details and display them on the page.</li>
             <li>Build technical solutions that require little knowledge for non-technical personnel for updates and maintenance.</li>
             <li>Rewrite urls with htaccess on apache servers (for both security and SEO purposes).</li>

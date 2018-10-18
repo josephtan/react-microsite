07c29a8ff6cbc4138b06153084e98e00149e2f76
josephtan <tantjernbit@gmail.com>
2018-10-19 01:36:05 +1100

    Copy change

--- src/components/views/portfolio.jsx
+++ src/components/views/portfolio.jsx
@@ -2,7 +2,7 @@ import PopupBox from "../popupBox";
 const Portfolio=()=> (
     <div className="page-portfolio has-text-centered">
         <h2>Portfolio</h2>
-        <p>Past private and commercial projects which I have worked on through the years.</p>
+        <p>Past private and commercial projects which I have worked on through the years as a front-end then to a full-stack developer.</p>
         <PopupBox/>
     </div>
 );

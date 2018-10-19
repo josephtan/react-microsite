decc8867a00623db739b7625529caea51fdfc24b
josephtan <tantjernbit@gmail.com>
2018-10-20 00:34:47 +1100

    add meaningful text to links for accesibility

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -58,13 +58,13 @@ class App extends Component {
                 <footer className="footer block__relative text-white">
                     <div className="has-text-centered">
                         <p>Web activities and external links that I can be contacted:</p>
-                        <ul className="no-liststyle list-inline white-link font-smaller">
-                            <li><a href="https://github.com/josephtan" target="_blank"><span className="font-fa-brands fa-github"></span></a></li>
-                            <li><a href="https://codepen.io/josephtan/"  target="_blank"><span className="font-fa-brands fa-codepen"></span></a></li>
-                            <li><a href="https://www.meetup.com/" target="_blank"><span className="font-fa-brands fa-meetup"></span></a></li>
-                            <li><a href="https://www.slack.com/" target="_blank"><span className="font-fa-brands fa-slack"></span></a></li>
-                            <li><a href="https://jsfiddle.net/user/JosephTan/fiddles/" target="_blank"><span className="font-fa-brands fa-fiddle"></span></a></li>
-                            <li><a href="https://www.linkedin.com/in/joetan83/" target="_blank"><span className="font-fa-brands fa-linkedin"></span></a></li>
+                        <ul className="no-liststyle list-inline white-link font-smaller external-links">
+                            <li><a href="https://github.com/josephtan" rel="noreferrer" target="_blank"><span className="font-fa-brands fa-github"></span><span className="link-text">Github</span></a></li>
+                            <li><a href="https://codepen.io/josephtan/" rel="noreferrer"  target="_blank"><span className="font-fa-brands fa-codepen"></span><span className="link-text">Codepen</span></a></li>
+                            <li><a href="https://www.meetup.com/" rel="noreferrer" target="_blank"><span className="font-fa-brands fa-meetup"></span><span className="link-text">Meetup</span> </a></li>
+                            <li><a href="https://www.slack.com/" rel="noreferrer" target="_blank"><span className="font-fa-brands fa-slack"></span><span className="link-text">Slack</span></a></li>
+                            <li><a href="https://jsfiddle.net/user/JosephTan/fiddles/" rel="noreferrer" target="_blank"><span className="font-fa-brands fa-fiddle"></span><span className="link-text">jsFiddle</span></a></li>
+                            <li><a href="https://www.linkedin.com/in/joetan83/" rel="noreferrer" target="_blank"><span className="font-fa-brands fa-linkedin"></span><span className="link-text">linkedIn</span></a></li>
                         </ul>
                     </div>
                     <div className="container">

f1cad398eeb9511059761da69d6c5965b6717dbb
josephtan <tantjernbit@gmail.com>
2019-05-28 11:16:52 +1000

    error page link update

--- src/js/views/error.js
+++ src/js/views/error.js
@@ -4,7 +4,7 @@ const Error =()=> (
     <div className="page-error has-text-centered">
         <h2 className="font-larger">Error 404 page not found!</h2>
         <p>Oops it seems you got lost. Click on the button below to get back to the homepage.</p>
-        <p> <a href="" className="btn no-underline">Back</a></p>
+        <p> <a href="/" className="btn no-underline">Back</a></p>
     </div>
 );
 
